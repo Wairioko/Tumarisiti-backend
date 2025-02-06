6056ebaf48a7f1b12db230414976cc785c9ee6d1
Wairioko <charlesmungai5@gmail.com>
2025-02-06 21:58:53 +0300

    invoice routes

--- src/invoices/routes/invoiceRoutes.mjs
+++ src/invoices/routes/invoiceRoutes.mjs
@@ -1,22 +1,26 @@
 import Router from "express"
-import { checkInvoice } from "../controller/checkInvoice.mjs";
 import { createInvoice } from "../controller/createInvoice.mjs";
 import multer from "multer";
 import { deleteInvoice } from "../controller/deleteInvoice.mjs";
 import { editInvoice } from "../controller/editInvoice.mjs";
+import { invoiceStatus } from "../controller/invoiceStatus.mjs";
+import { verifyJWT } from "../../../middlewares.mjs";
 
 
 
 const router = Router();
 
+
 const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-const upload = multer({storage})
 
-router.get('/api/invoice/check', checkInvoice )
-router.post('/api/invoice/upload', upload.fileFilter, createInvoice)
-router.delete('/api/invoice/delete', deleteInvoice)
-router.put('/api/invoice/update', editInvoice)
+router.post('/api/invoice/upload', verifyJWT,  upload.single('file'), createInvoice);
+// router.get('/api/invoices/check', verifyJWT ,checkInvoice )
+router.delete('/api/invoice/delete',verifyJWT, deleteInvoice)
+router.put('/api/invoice/update', verifyJWT ,editInvoice)
+router.get('/api/invoices/status', verifyJWT,invoiceStatus)
+
 
 
 

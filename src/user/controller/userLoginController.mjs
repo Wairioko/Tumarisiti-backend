1390aec5fdc73dfa43f201d6b981d76e6697ff7c
Wairioko <charlesmungai5@gmail.com>
2025-02-06 21:59:33 +0300

    user login controller

--- src/user/controller/userLoginController.mjs
+++ src/user/controller/userLoginController.mjs
@@ -6,37 +6,34 @@ import { getCookieConfig } from "../../../constants.mjs";
 
 dotenv.config()
 
+
 export const companyLogin = async (req, res) => {
-    const {KRApin, password} = req.body;
-    if(!KRApin || !password){
-        return res.status(400).send("Please fill missing data")
+    const { KRApin, password } = req.body;
+    
+
+    if (!KRApin || !password) {
+        return res.status(400).send("Please fill missing data");
     }
 
-    try{
-        const findCompany = companyModel.findOne({KRApin:KRApin});
-        if(!findCompany){
-            res.status(404).send("Company not found. Register to continue using service ")
-            return res.redirect('/company/registration')
+    try {
+        const findCompany = await companyModel.findOne({ KRApin: KRApin });
+        if (!findCompany) {
+            return res.status(404).send("Company not found. Register to continue using service");
         }
 
-        const hashedPassword = findCompany.password
-        const passwordValidation = await bcrypt.compareSync(password, hashedPassword);
-        if(!passwordValidation){
-            return res.status(400).send("Error! Please input correct password")
+        const passwordValidation = await bcrypt.compare(password, findCompany.password);
+        if (!passwordValidation) {
+            return res.status(400).send("Error! Please input correct password");
         }
+
         const tokenSecret = process.env.AuthTokenSecret;
-        const token = jwt.sign(KRApin, tokenSecret,{
-            expiresIn: '1h'
-        } )
+        const token = jwt.sign({ KRApin }, tokenSecret, { expiresIn: '1h' });
 
-        res.cookie('authToken', token, getCookieConfig())
-        res.status(201).send("Successfully logged in")
-        res.redirect('/')
+        res.cookie('authToken', token, getCookieConfig());
+        return res.status(200).send("Successfully logged in");
 
-    }catch(error){
-        console.error("loggging in ", error)
-        return res.status(500).send("Error logging in. Please try again later")   
+    } catch (error) {
+        console.error("Error logging in", error);
+        return res.status(500).send("Error logging in. Please try again later");
     }
-}
-
-
+};

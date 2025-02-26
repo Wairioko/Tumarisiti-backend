750fe0109ff062b9bb33c1734c6e148e532d653d
Wairioko <charlesmungai5@gmail.com>
2025-02-26 19:01:42 +0300

    routes for company

--- src/user/routes/companyRoutes.mjs
+++ src/user/routes/companyRoutes.mjs
@@ -1,11 +1,15 @@
 import { companyLogin } from "../controller/userLoginController.mjs";
 import { companySignup } from "../controller/userSignUpController.mjs";
+import { checkAuth } from "../controller/authStatus.mjs";
 import {Router} from 'express'
+import { logoutController } from "../controller/logoutController.mjs";
 
 const router = Router();
 
 router.post('/api/company/registration', companySignup)
 router.post('/api/company/login', companyLogin)
+router.get('/api/auth/status', checkAuth)
+router.post('/api/auth/logout', logoutController)
 
 
 export default router;
